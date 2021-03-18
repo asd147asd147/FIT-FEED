6581224cbb34e37bb518f30e23c85cdcdbdc2e7a
WonJune Choi <55697983+asd147asd147@users.noreply.github.com>
2021-03-18 22:10:14 +0900

    Revert "Feature (#10)"
    This reverts commit 77d212e552dcb6d09ae4e39c48348bb5e73071d3.

--- fit-feed/src/App.js
+++ fit-feed/src/App.js
@@ -1,8 +1,24 @@
-import Routes from './Routes'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Routes />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello FIT-FEED
+        </a>
+      </header>
+    </div>
   );
 }
 
