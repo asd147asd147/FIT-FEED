2b4309c49fe24c5872759c6b51f763e38c7ee014
seung <38664481+seunggil1@users.noreply.github.com>
2021-04-14 22:15:08 +0900

    .

---
+++ Python/facebook.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup as BS4
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import datetime
+import time
+import re
+import json
+
+webdriverLocation = 'Python\\Chrome_88.0.4324.96\\chromedriver.exe'
+
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument('disable-gpu')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
+options.add_experimental_option("prefs",{"profile.default_content_setting_values.notifications" : 2})
+driver = webdriver.Chrome(webdriverLocation, chrome_options = options)
+
+#페이스북 아이디 비번
+email = ''
+password = ''
+
+# 로그인
+driver.get('https://www.facebook.com')
+driver.find_element_by_xpath('//*[@id="email"]').click()
+driver.find_element_by_xpath('//*[@id="email"]').send_keys(email)
+driver.find_element_by_xpath('//*[@id="pass"]').send_keys(password)
+driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[1]/div/div/div/div[2]/div/div[1]/form/div[2]/button').click()
+driver.implicitly_wait(5)
+
+
+
+# 게시글 불러오기를 위한 5초간 스크롤 다운 실행
+start = datetime.datetime.now()
+end = start + datetime.timedelta(seconds=5)
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    time.sleep(1)
+    if datetime.datetime.now() > end:
+        break
+html = driver.page_source
+soup = BS4(html,'html.parser')
+
+# f = open("Python\\Facebook.html", 'r', encoding="utf8")
+# soup = BS4(f,'html.parser')
+
+data = []
+content_list = soup.find_all(attrs={'data-pagelet': re.compile('^FeedUnit')})
+for content in content_list:
+    # 그룹 페이지에서 글쓰는건 못불러옴.
+    pageName = content.find('strong')
+    if pageName:
+        pageName = pageName.getText()
+    else :
+        pageName = ""
+    pageImg = content.find('g',attrs={'mask':re.compile(r'jsc_.+?\)')}).find('image')
+    if pageImg:
+        pageImg = pageImg.attrs['xlink:href']
+    else:
+        pageImg = ""
+
+    mainText = ""
+    mainImg = []
+    for textIter in content.find_all('div',attrs={'dir':'auto', 'style':re.compile('text-align:.*')}):
+        mainText += textIter.getText() + '\n'
+    for imgIter in content.find_all('img',attrs={'alt': re.compile('.+')}):
+        mainImg.append({
+            "src" : imgIter.attrs['src'],
+            # "height": imgIter.attrs['height'],
+            # "width" : imgIter.attrs['width']
+        })
+    dataIter = {
+        "pageName" : pageName,
+        "pageImage" : {
+            "type" : "img",
+            "src" : pageImg,
+            # 40 -> 40px
+            "height": 40,
+            "width" : 40,
+        },
+        "description" : mainText,
+        "media" : mainImg
+    }
+    data.append(dataIter)
+
+jsonData = {}
+jsonData["data"] = data
+
+with open("facebook.json", "w", encoding='utf8') as json_file:
+    json.dump(data, json_file, indent=4, ensure_ascii = False)
+
---
+++ Python/instagram.py
@@ -0,0 +1,129 @@
+from bs4 import BeautifulSoup as BS4
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.alert import Alert
+import datetime
+import time
+import re
+import json
+import orderedSet
+
+def login(web, email, pw):
+    web.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(email)
+    web.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(pw)
+    web.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+
+def loginWithFacebook(web, email, pw):
+    web.find_element_by_xpath('//*[@id="loginForm"]/div/div[5]/button').click()
+    web.find_element_by_xpath('//*[@id="email"]').send_keys(email)
+    web.find_element_by_xpath('//*[@id="pass"]').send_keys(pw)
+    web.find_element_by_xpath('//*[@id="loginbutton"]').click()
+    
+webdriverLocation = 'Python\\Chrome_88.0.4324.96\\chromedriver.exe'
+
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument('disable-gpu')
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36")
+options.add_experimental_option("prefs",{"profile.default_content_setting_values.notifications" : 2})
+driver = webdriver.Chrome(webdriverLocation, options = options)
+
+#인스타 아이디 비번
+email = ''
+password = ''
+
+# 로그인
+driver.get('https://www.instagram.com/')
+driver.implicitly_wait(3)
+loginWithFacebook(driver,email,password)
+driver.implicitly_wait(3)
+while True:
+    if driver.current_url == 'https://www.instagram.com/':
+        break
+    time.sleep(1)
+try:
+    Alert(driver).dismiss()
+except:
+    pass
+
+driver.implicitly_wait(3)
+driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+driver.implicitly_wait(3)
+driver.refresh()
+data = []
+content_list = orderedSet.OrderedSet()
+
+
+# 게시글 불러오기를 위한 5초간 스크롤 다운 실행
+start = datetime.datetime.now()
+end = start + datetime.timedelta(seconds=5)
+while True:
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    driver.implicitly_wait(3)
+
+    html = driver.page_source
+    soup = BS4(html,'html.parser')
+
+    for iter in soup.find_all('article'):
+        content_list.add(iter)
+
+    if datetime.datetime.now() > end:
+        break
+
+data = []
+
+for content in content_list:
+    header = content.contents[0]
+    body = content.contents[2]
+    comment = content.contents[3]
+
+    pageImage = header.find_all('img',attrs={'data-testid':'user-avatar'})[0].attrs['src']
+    pageName = header.find_all('a',attrs={'tabindex':'0'})
+    if pageName[0].text != '':
+        pageName = pageName[0].text
+    else :
+        pageName = pageName[1].text
+    #pageName = header.find_all('a',attrs={'tabindex':'0'})[0].text
+
+    location = ""
+    like = ""
+    for iter in header.find_all('a',attrs={'tabindex':'0'}):
+        if 'href' in iter.attrs and 'locations' in iter.attrs['href']:
+            location = iter.text
+        if 'href' in iter.attrs and 'liked_by' in iter.attrs['href']:
+            like = iter.text
+        
+    
+    mainText = ""
+    mainImg = []
+    for iter in body.find_all('img',{'src':re.compile(r'.*')}):
+        if 'src' in iter.attrs:
+            mainImg.append(iter.attrs['src'])
+    
+    for iter in comment.find_all('a',attrs={'tabindex':'0'}):
+        if 'href' in iter.attrs and 'liked_by' in iter.attrs['href']:
+            like = iter.text
+    mainText = comment.find('div',{'data-testid':'post-comment-root'}).text
+
+    dataIter = {
+        "pageName" : pageName,
+        "pageImage" : {
+            "src" : pageImage,
+            # 40 -> 40px
+            "height": 150,
+            "width" : 150,
+        },
+        "location": location,
+        "description" : mainText,
+        "like":like,
+        "media" : mainImg
+    }
+    data.append(dataIter)
+
+jsonData = {}
+jsonData["data"] = data
+
+with open("instagram.json", "w", encoding='utf8') as json_file:
+    json.dump(data, json_file, indent=4, ensure_ascii = False)
+
